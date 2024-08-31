8820f12ad58ed30e9f28faa7386cd10c9466feff
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-31 14:34:28 +0500

    multer storage changed to cloundinary

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,27 +1,27 @@
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
-// import { v2 as cloudinary } from 'cloudinary';
-// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-// // Configure Cloudinary
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// });
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-// // Set up Cloudinary storage engine for multer
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params: {
-//     folder: 'uploads', // Folder in your Cloudinary account
-//     format: async (req, file) => 'jpg', // Force format to jpg
-//     public_id: (req, file) => `${Date.now()}_${file.originalname}`
-//   },
-// });
+// Set up Cloudinary storage engine for multer
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads', // Folder in your Cloudinary account
+    format: async (req, file) => 'jpg', // Force format to jpg
+    public_id: (req, file) => `${Date.now()}_${file.originalname}`
+  },
+});
 
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 const foodRouter = express.Router();
 
